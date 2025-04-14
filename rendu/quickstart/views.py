121871dbae44994c9ca9efd80187df4ede5fd730
Léora Chriqui <leora.chriqui@gmail.com>
2025-04-14 16:07:52 +0200

    wip

---
+++ rendu/quickstart/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from rendu.quickstart.serializers import GroupSerializer, UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """ 
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['GET'])
+def health_check(request):
+    """
+    Simple health check endpoint.
+    """
+    return Response({"test": "hello world"})
